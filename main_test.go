61bae4d1a465bedc92ae9c6073d702ef466289ad
Nihz <nihz.tichondrius@gmail.com>
2018-01-19 14:08:25 -0500

    Fixed the test.

--- main_test.go
+++ main_test.go
@@ -17,7 +17,7 @@ func TestHandler(t *testing.T) {
 			// Test that the handler responds with the correct response
 			// when a valid name is provided in the HTTP body
 			request: events.APIGatewayProxyRequest{Body: "There"},
-			expect:  "Hello There",
+			expect:  "Super hello There",
 			err:     nil,
 		},
 		{
